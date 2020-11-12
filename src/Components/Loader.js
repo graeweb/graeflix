faf6721a20520f3c32cfcdb036505e082893efca
Grae <63225431+graeweb@users.noreply.github.com>
2020-11-12 16:27:49 +0900

    로딩 애니메이션 수정

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -16,18 +16,22 @@ display: inline-block;
   border-radius: 15px;
   background-color: #4b9cdb;
   &:nth-last-child(1) {
-    animation: loadingA .5s .2s infinite;
+    animation: loadingA .4s .1s infinite;
   }
   &:nth-last-child(2) {
-    animation: loadingA .5s .4s infinite;
+    animation: loadingA .4s .3s infinite;
   }
   &:nth-last-child(3) {
-    animation: loadingA .5s 0s infinite;
+    animation: loadingA .4s 4s infinite;
+  }
+  &:nth-last-child(4) {
+    animation: loadingA .4s 2s infinite;
   }
 `;
 export default () => (
   <Container>
       <div class="load-1">
+        <Line></Line>
         <Line></Line>
         <Line></Line>
         <Line></Line>
